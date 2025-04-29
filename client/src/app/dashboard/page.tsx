874dbda4da84a5dc05ece1afb821dc4cde7c9195
Hiren <hiren@prishusoft.com>
2025-04-29 20:34:28 +0530

    Fix scrollbar issue

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -120,10 +120,8 @@ export default function Dashboard() {
 
   return (
     <>
-      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
-     
-        <div className="flex items-center justify-between space-y-2">
-          
+      <div className="hidden h-auto flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">          
           <div>
             <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
             <p className="text-muted-foreground">
